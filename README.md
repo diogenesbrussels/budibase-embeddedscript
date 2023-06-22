76033a502fa22680a9f3be352adae982112f1afa
Olivier@Diogenes <olivier@diogenes.brussels>
2023-06-22 17:31:51 +0200

    First release

--- README.md
+++ README.md
@@ -1,20 +1,38 @@
 # Budibase-embeddedscript
-This is a readme for your new Budibase plugin.
+
+This append scripts to your current screen and use a table to store them.
 
 # Description
-Embed js scripts from your DB into your app
+
+Embed js scripts from your databse into your app
 
 Find out more about [Budibase](https://github.com/Budibase/budibase).
 
 ## Instructions
 
-To build your new  plugin run the following in your Budibase CLI:
+Create a table with the following columns:
+
+- content (text) JS code
+- collection (options or text) Collection name to group scripts
+- parent (options with values: parent, body, component) Where the script should be appended
+- inBuilder (boolean) Does the script execute in Builder
+
+Add the component, select your newly created table and filter by collection name if needed.
+
+## Known issues
+
+- Handlebars are parsed but can't access the live Budibase context
+
+## Instructions for devs
+
+To build your new plugin run the following in your Budibase CLI:
+
 ```
 budi plugins --build
 ```
 
 You can also re-build everytime you make a change to your plugin with the command:
+
 ```
 budi plugins --watch
 ```
-
--- src/Component.svelte
+++ src/Component.svelte
@@ -1,12 +1,59 @@
 <script>
-  import { getContext } from "svelte"
+  import { getContext } from "svelte";
+  import { onMount } from "svelte";
+  import { processStringSync } from "@budibase/string-templates";
 
-  export let text
+  export let collection, table;
 
-  const { styleable } = getContext("sdk")
-  const component = getContext("component")
+  const { styleable, API } = getContext("sdk");
+  const component = getContext("component");
+  const { builderStore } = getContext("sdk");
+  const inBuilder = $builderStore.inBuilder;
+
+  onMount(async () => {
+    let scriptContainer = document.getElementById("scriptContainer");
+
+    let scriptCollection = await API.post({
+      url: `/api/` + table.tableId + `/search`,
+      body: { query: { equal: { "1:collection": collection } } },
+    });
+
+    for (const scriptRow of scriptCollection.rows) {
+      let parentElement;
+      let script = document.createElement("script");
+      script.innerHTML = processStringSync(scriptRow.content, component);
+
+      if (inBuilder && scriptRow.inBuilder) {
+        if (scriptRow.parent == "head") {
+          parentElement = document.head;
+        } else if (scriptRow.parent == "body") {
+          parentElement = document.body;
+        } else if (scriptRow.parent == "component") {
+          parentElement = scriptContainer;
+        }
+        parentElement.appendChild(script);
+      }
+
+      if (!inBuilder) {
+        if (scriptRow.parent == "head") {
+          parentElement = document.head;
+        } else if (scriptRow.parent == "body") {
+          parentElement = document.body;
+        } else if (scriptRow.parent == "component") {
+          parentElement = scriptContainer;
+        }
+        parentElement.appendChild(script);
+      }
+    }
+  });
 </script>
 
 <div use:styleable={$component.styles}>
-  This is a custom component. The text setting is: {text}.
+  {#if inBuilder}
+    <div>
+      📜 This component embeds your script collection here. This message is only
+      visible in the builder. 📜
+    </div>
+  {/if}
+  <div id="scriptContainer" />
 </div>
